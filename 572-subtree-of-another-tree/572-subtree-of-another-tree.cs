e5c360071a8749c9a03d39b656e098cea8989c76
Ravi Kumar <ravi.kc12@gmail.com>
2022-01-12 19:01:36 +0530

    Time: 153 ms (22.54%), Space: 39.2 MB (95.64%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cs
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cs
@@ -16,9 +16,9 @@ public class Solution {
         if (n1 == null && n2 == null) return true;
         if (n1 == null || n2 == null) return false;
         
-        if (n1.val != n2.val) return false;
+        // if (n1.val != n2.val) return false;
         
-        return Same(n1.left, n2.left) && Same(n1.right, n2.right);
+        return n1.val == n2.val && Same(n1.left, n2.left) && Same(n1.right, n2.right);
     }
     
     
