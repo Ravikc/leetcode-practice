fec1ac208ab0d3e3955513ce832d68b30decf5a7
Ravi Kumar <ravi.kc12@gmail.com>
2022-02-07 18:06:56 +0530

    Time: 84 ms (57.35%), Space: 15.5 MB (9.26%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -3,7 +3,9 @@ class Solution:
         max = arr[1]
         maxIndex = 1
         for index in range(1, len(arr) - 1, 1):
-            if arr[index] > max:
+            if arr[index] < max:
+                return maxIndex
+            elif arr[index] > max:
                 max = arr[index]
                 maxIndex = index
         
