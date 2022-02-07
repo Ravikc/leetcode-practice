2afb05fbe5f96f7aaf2977fe0bd0f2b6be334c78
Ravi Kumar <ravi.kc12@gmail.com>
2022-02-07 18:05:08 +0530

    Time: 80 ms (64.47%), Space: 15.3 MB (58.71%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        max = arr[1]
+        maxIndex = 1
+        for index in range(1, len(arr) - 1, 1):
+            if arr[index] > max:
+                max = arr[index]
+                maxIndex = index
+        
+        return maxIndex
+        
