32733fded946be4b8adfafc9460b737dd6164fc7
Ravi Kumar <ravi.kc12@gmail.com>
2022-04-04 14:05:25 +0530

    Time: 55 ms (33.03%), Space: 13.9 MB (17.89%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,38 @@
+class Solution:
+    def swap(self, nums, left, right):
+        temp = nums[left]
+        nums[left] = nums[right]
+        nums[right] = temp
+        
+    def sortColors(self, nums: List[int]) -> None:
+        left = 0
+        right = len(nums) - 1
+        
+        while left < len(nums) and nums[left] == 0:
+            left += 1
+        
+        while right >= 0 and nums[right] == 2:
+            right -= 1
+            
+        curr = left
+        while curr <= right:
+            swapped = False
+            if nums[curr] == 0:
+                self.swap(nums, left, curr)
+                swapped = True                
+            
+            if nums[curr] == 2:
+                self.swap(nums, curr, right)
+                swapped = True
+                
+                    
+            while left < len(nums) and nums[left] == 0:
+                left += 1
+            
+            while right >= 0 and nums[right] == 2:
+                right -= 1
+            
+            if not swapped:
+                curr += 1
+            else:
+                curr = left
