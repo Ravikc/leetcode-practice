125c0f8bc758907c6f16638717d84c1d595dfae8
Ravi Kumar <ravi.kc12@gmail.com>
2022-04-19 18:42:02 +0530

    Time: 375 ms (17.08%), Space: 14.2 MB (97.02%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findRoot(self, roots, x):
+        while roots[x] != x:
+            x = roots[x]
+            
+        return x
+    
+    def union(self, roots, x, y):
+        rootX = self.findRoot(roots, x)
+        rootY = self.findRoot(roots, y)
+        if rootX == rootY:
+            return
+        
+        roots[rootY] = rootX
+        
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        roots = [i for i in range(len(isConnected))]
+        for i in range(0, len(isConnected)):
+            for j in range(i + 1, len(isConnected)):
+                if isConnected[i][j] == 1:
+                    self.union(roots, i, j)
+                  
+        print(roots)
+        provinces = set([self.findRoot(roots, i) for i in range(len(isConnected))])
+        return len(provinces)
+        
+            
