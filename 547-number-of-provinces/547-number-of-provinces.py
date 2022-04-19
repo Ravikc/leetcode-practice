3505092f6e3773c76fe7ffd979751afbd0f476d5
Ravi Kumar <ravi.kc12@gmail.com>
2022-04-19 18:49:25 +0530

    Time: 257 ms (57.08%), Space: 14.4 MB (54.01%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -5,20 +5,29 @@ class Solution:
             
         return x
     
-    def union(self, roots, x, y):
+    def union(self, roots, ranks, x, y):
         rootX = self.findRoot(roots, x)
         rootY = self.findRoot(roots, y)
         if rootX == rootY:
             return
         
-        roots[rootY] = rootX
+        if ranks[rootX] > ranks[rootY]:
+            roots[rootY] = rootX
+        elif ranks[rootX] < ranks[rootY]:
+            roots[rootX] = rootY
+        else:
+            roots[rootY] = rootX
+            ranks[rootX] += 1
+        
+        
         
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         roots = [i for i in range(len(isConnected))]
+        ranks = [1 for i in range(len(isConnected))]
         for i in range(0, len(isConnected)):
             for j in range(i + 1, len(isConnected)):
                 if isConnected[i][j] == 1:
-                    self.union(roots, i, j)
+                    self.union(roots, ranks, i, j)
                   
         provinces = set([self.findRoot(roots, i) for i in range(len(isConnected))])
         return len(provinces)
