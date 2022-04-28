3f084137da1af56dbb978fbe306a53897ce140fd
Ravi Kumar <ravi.kc12@gmail.com>
2022-04-28 19:40:01 +0530

    Time: 22 ms (99.08%), Space: 13.9 MB (66.21%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def get(self, row, col):
+        if row == col or col == 0:
+            return 1
+        
+        return self.get(row - 1, col) + self.get(row - 1, col - 1)
+
+    
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        
+        if rowIndex == 1:
+            return [1, 1]
+        
+        prevRow = self.getRow(rowIndex - 1)
+        thisRow = [1]
+        for i in range(1, rowIndex):
+            thisRow.append(prevRow[i - 1] + prevRow[i])
+            
+        thisRow.append(1)
+        return thisRow
+        
